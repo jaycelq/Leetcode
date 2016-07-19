996141e3d635e81c3855f5a22e392237d2244f4d
jaycelq <jaycelq@gmail.com>
2016-07-19 23:32:56 +0800

    day two

---
+++ C++/isValid.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        map<char, char> pair_map;
+        pair_map[')'] = '(';
+        pair_map[']'] = '[';
+        pair_map['}'] = '{';
+        for (int i = 0; i < s.size(); i++)
+        {
+            char c = s[i];
+            char pair;
+            switch(c)
+            {
+            case '(':
+            case '[':
+            case '{':
+                stack.emplace(c);
+                break;
+            case ')':
+            case ']':
+            case '}':
+                if (stack.empty())
+                    return false;
+                pair = stack.top();
+                stack.pop();
+                if (pair != pair_map[c])
+                {
+                    return false;
+                }
+                break;
+            default:
+                return false;
+            }
+        }
+
+        return stack.empty();
+    }
+};
--- C++/kSmallestPairs.cpp
+++ C++/kSmallestPairs.cpp
@@ -58,3 +58,49 @@ public:
         return result;
     }
 };
+
+class Solution2 {
+public:
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k)
+    {
+        vector<pair<int, int>> result;
+
+        if (nums1.empty() || nums2.empty() || k <= 0)
+        {
+            return result;
+        }
+
+        auto comp = [&nums1, &nums2](pair<int, int> lhs, pair<int, int> rhs) {
+            return nums1[lhs.first] + nums2[lhs.second] > nums1[rhs.first] + nums2[rhs.second];
+        };
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> min_heap(comp);
+
+        min_heap.emplace(0, 0);
+
+        while (min_heap.empty() == false && k > 0)
+        {
+            auto idx_pair = min_heap.top();
+            min_heap.pop();
+            result.emplace_back(nums1[idx_pair.first], nums2[idx_pair.second]);
+
+            if (idx_pair.first + 1 < nums1.size())
+            {
+                min_heap.emplace(idx_pair.first + 1, idx_pair.second);
+            }
+            if (idx_pair.first == 0 && idx_pair.second + 1 < nums2.size())
+            {
+                min_heap.emplace(idx_pair.first, idx_pair.second+1);
+            }
+            k--;
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+
+}
+
